c1d3eacf7608549d76acc3d0526d5079c7c059f0
Hardik-Ahi <ahi.hardik@gmail.com>
2025-10-31 21:19:46 +0530

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = var.AWS_REGION
+}
+
+resource "aws_iam_role_policy" "ecrAccessPolicy" {
+  role = aws_iam_role.ecrAccess.name
+  policy = data.aws_iam_policy_document.instance_ecr_access_policy.json
+}
+
+resource "aws_iam_role" "ecrAccess" {
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json  // just 'sts:AssumeRole' permission
+  name = "ecrAccessRole"
+}
+
+resource "aws_iam_instance_profile" "instanceProfile" {
+  role = aws_iam_role.ecrAccess.name
+}
+
+resource "aws_instance" "demo" {
+  ami = var.AWS_EC2_AMI
+  instance_type = var.AWS_EC2_TYPE
+  key_name = aws_key_pair.keyPair.key_name
+  vpc_security_group_ids = [ aws_security_group.allow_traffic.id ]
+  iam_instance_profile = aws_iam_instance_profile.instanceProfile.name
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install -y docker java-21-amazon-corretto
+  sudo service docker start
+  sudo systemctl enable docker
+  java -version
+  sudo usermod -aG docker ec2-user
+  sudo docker pull ${var.DOCKER_REPO}/${var.DOCKER_IMG}:latest
+  sudo docker run -d -p 9090:9090 ${var.DOCKER_REPO}/${var.DOCKER_IMG}:latest
+  EOF
+}
+
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_traffic"
+  description = "Allow all inbound traffic"
+}
+
+resource "aws_key_pair" "keyPair" {
+  key_name = "public-key-ec2"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBkEfAiTo4isYALYxDm5Uv04VZ3TLwfI1luOTMugIee ahiha@AcerNitro5"
+}
+
+resource "aws_security_group_rule" "ingress" {
+  type = "ingress"
+  protocol = -1
+  security_group_id = aws_security_group.allow_traffic.id
+  from_port = 0  # start of port range
+  to_port = 65535  # end of port range
+  cidr_blocks = [ "0.0.0.0/0" ]
+  ipv6_cidr_blocks = [ "::/0" ]
+}
+
+resource "aws_security_group_rule" "egress" {
+  type = "egress"
+  protocol = -1
+  security_group_id = aws_security_group.allow_traffic.id
+  from_port = 0  # start of port range
+  to_port = 65535  # end of port range
+  cidr_blocks = [ "0.0.0.0/0" ]
+  ipv6_cidr_blocks = [ "::/0" ]
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "AWS_ID" {
+  default = "950332950980"
+}
+
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "AWS_EC2_AMI" {
+  default = "ami-0f9708d1cd2cfee41"
+}
+
+variable "AWS_EC2_TYPE" {
+  default = "t3.micro"
+}
+
+variable "DOCKER_REPO" {
+  default = "ahihardik11"
+}
+
+variable "DOCKER_IMG" {
+  default = "spring-app"
+}
